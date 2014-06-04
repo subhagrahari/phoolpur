2d8530f6a262a26768b25464c694788d69af7f3d
subhagrahari <saurabhkumargupta87@gmail.com>
2014-06-05 00:35:55 +0530

    upd

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 die("saurabh");
 ?>
+
+
+Create a new repository on the command line
+
+touch README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/subhagrahari/phoolpur.git
+git push -u origin master
+
+Push an existing repository from the command line
+
+git remote add origin https://github.com/subhagrahari/phoolpur.git
+git push -u origin master
+
++++++++++++++++++++++++++++++++++++++++
+git checkout -b (new branch name)
